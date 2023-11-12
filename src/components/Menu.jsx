f2f6054b0679f59bdf3b2de1c7c996e4978da69c
Shamshad Alam <70852128+Shamshad25@users.noreply.github.com>
2023-11-12 17:42:12 +0530

    post upload component completed

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,48 +1,65 @@
-const Menu = () => {
-  const posts = [
-    {
-      albumId: 1,
-      id: 1,
-      title: "accusamus beatae ad facilis cum similique qui sunt",
-      url: "https://via.placeholder.com/600/92c952",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-      albumId: 1,
-      id: 2,
-      title: "reprehenderit est deserunt velit ipsam",
-      url: "https://via.placeholder.com/600/771796",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-      albumId: 1,
-      id: 3,
-      title: "officia porro iure quia iusto qui ipsa ut modi",
-      url: "https://via.placeholder.com/600/24f355",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-      albumId: 1,
-      id: 4,
-      title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
-      url: "https://via.placeholder.com/600/d32776",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-    {
-      albumId: 1,
-      id: 5,
-      title: "natus nisi omnis corporis facere molestiae rerum in",
-      url: "https://via.placeholder.com/600/f66b97",
-      body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
-    },
-  ];
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const Menu = ({ cat }) => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(`/posts/?cat=${cat}`);
+        setPosts(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchData();
+  }, [cat]);
+
+  // const posts = [
+  //   {
+  //     albumId: 1,
+  //     id: 1,
+  //     title: "accusamus beatae ad facilis cum similique qui sunt",
+  //     url: "https://via.placeholder.com/600/92c952",
+  //     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  //   },
+  //   {
+  //     albumId: 1,
+  //     id: 2,
+  //     title: "reprehenderit est deserunt velit ipsam",
+  //     url: "https://via.placeholder.com/600/771796",
+  //     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  //   },
+  //   {
+  //     albumId: 1,
+  //     id: 3,
+  //     title: "officia porro iure quia iusto qui ipsa ut modi",
+  //     url: "https://via.placeholder.com/600/24f355",
+  //     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  //   },
+  //   {
+  //     albumId: 1,
+  //     id: 4,
+  //     title: "culpa odio esse rerum omnis laboriosam voluptate repudiandae",
+  //     url: "https://via.placeholder.com/600/d32776",
+  //     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  //   },
+  //   {
+  //     albumId: 1,
+  //     id: 5,
+  //     title: "natus nisi omnis corporis facere molestiae rerum in",
+  //     url: "https://via.placeholder.com/600/f66b97",
+  //     body: "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla",
+  //   },
+  // ];
 
   return (
     <div className="menu">
       <h1>Other post you may like</h1>
       {posts.map((post) => (
         <div className="post" key={post.id}>
-          <img src={post.url} alt="" />
+          <img src={`../upload/${post.img}`} alt="" />
           <h2>{post.title}</h2>
           <button>Read More</button>
         </div>
--- src/pages/SinglePost.jsx
+++ src/pages/SinglePost.jsx
@@ -1,5 +1,5 @@
 import { FaRegPenToSquare, FaRegTrashCan } from "react-icons/fa6";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import Menu from "../components/Menu";
 import { useContext, useEffect, useState } from "react";
 import axios from "axios";
@@ -10,6 +10,8 @@ const SinglePost = () => {
   const [post, setPost] = useState([]);
 
   const location = useLocation();
+
+  const navigate = useNavigate();
 
   const postId = location.pathname.split("/")[2];
 
@@ -26,25 +28,39 @@ const SinglePost = () => {
     };
     fetchData();
   }, [postId]);
+
+  const handleDelete = async () => {
+    try {
+      await axios.delete(`/posts/${postId}`);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getText = (html) => {
+    const doc = new DOMParser().parseFromString(html, "text/html");
+    return doc.body.textContent;
+  };
   return (
     <div className="single">
       <div className="content">
-        <img src={post?.img} alt="" />
+        <img src={`../upload/${post?.img}`} alt="" />
         <div className="user">
-          <img src="https://via.placeholder.com/600/f66b97" alt="" />
+          {post.userImg && <img src={post.userImg} alt="" />}
           <div className="info">
             <span>{post.username}</span>
             <p>Posted {moment(post.date).fromNow()}</p>
           </div>
 
-          {currentUser.usename === post.username && (
+          {currentUser.username === post.username && (
             <div className="edit">
               <div className="edit-btn">
-                <Link to={`/write?edit=2`}>
+                <Link to={`/write?edit=2`} state={post}>
                   <FaRegPenToSquare color="white" />
                 </Link>
               </div>
-              <div className="delete-btn">
+              <div className="delete-btn" onClick={handleDelete}>
                 <Link>
                   <FaRegTrashCan color="white" />
                 </Link>
@@ -53,9 +69,9 @@ const SinglePost = () => {
           )}
         </div>
         <h1>{post.title}</h1>
-        {post.desc}
+        {getText(post.desc)}
       </div>
-      <Menu />
+      <Menu cat={post.cat} />
     </div>
   );
 };
